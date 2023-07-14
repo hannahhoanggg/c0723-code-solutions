9be28438c2da00bffac72cc26e715165c7d53c7f
Hannah Hoang <hannahhoanggg@gmail.com>
2023-07-14 20:47:05 +0000

    Completed javascript-loops-practice exercise.

---
+++ javascript-loops-practice/filter-out-nulls.js
@@ -0,0 +1,11 @@
+/* exported filterOutNulls */
+
+function filterOutNulls(values) {
+  const newArray = [];
+  for (let i = 0; i < values.length; i++) {
+    if (values[i]) {
+      newArray.push(values[i]);
+    }
+  }
+  return newArray;
+}
---
+++ javascript-loops-practice/get-indexes.js
@@ -0,0 +1,10 @@
+/* exported getIndexes */
+
+function getIndexes(array) {
+  const newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(i);
+  }
+  return newArray;
+}
