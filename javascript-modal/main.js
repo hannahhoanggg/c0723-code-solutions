518afc2f1e227cbe9e220a9ee245b344ca72384f
Hannah Hoang <hannahhoanggg@gmail.com>
2023-07-26 16:07:15 +0000

    work in progress.

---
+++ javascript-modal/main.js
@@ -0,0 +1,4 @@
+const $openButton = document.querySelector('.open-button');
+// const $popupContainer = document.querySelector('.popup-container');
+
+$openButton.addEventListener('click', function () {});
