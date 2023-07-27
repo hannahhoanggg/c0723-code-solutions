0b82ad05f2a3e3100a318f84e713b1204d5c8057
Hannah Hoang <hannahhoanggg@gmail.com>
2023-07-27 20:40:04 +0000

    Made some code changes.

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -4,7 +4,7 @@ function taskList(event) {
   console.log('event.target: ', event.target);
   console.log('element tag name: ', event.target.tagName);
 
-  if (event.target.tagName === 'button') {
+  if (event.target.tagName === 'BUTTON') {
     const $taskListItem = event.target.closest('.task-list-item');
     console.log($taskListItem);
     $taskListItem.remove();
