93c536a5b319fba912851eb248ad88250b308119
Hannah Hoang <hannahhoanggg@gmail.com>
2023-07-30 06:09:07 +0000

    Completed javascript-modal exercise.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,4 +1,16 @@
+// Locating elements (any html tags ex: div, button, span)
+const $popupContainer = document.querySelector('.popup-container');
 const $openButton = document.querySelector('.open-button');
-// const $popupContainer = document.querySelector('.popup-container');
+const $buttonClose = document.querySelector('.button-close');
 
-$openButton.addEventListener('click', function () {});
+// Adding event listener to element (waiting for an event & firing an action)
+$openButton.addEventListener('click', openModal);
+$buttonClose.addEventListener('click', closedModal);
+
+// Actions to perform
+function openModal(event) {
+  $popupContainer.className = 'popup-container';
+}
+function closedModal(event) {
+  $popupContainer.className = 'popup-container hidden';
+}
