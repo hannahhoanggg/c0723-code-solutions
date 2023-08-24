85a50e37afbff381bf384f245627c1ac60af11b1
Hannah Hoang <hannahhoanggg@gmail.com>
2023-08-24 23:18:48 +0000

    Edited stuff.

--- react-props-and-expressions/hannah-app/src/CustomButton.jsx
+++ react-props-and-expressions/hannah-app/src/CustomButton.jsx
@@ -2,5 +2,5 @@ export default function CustomButton(props) {
   const buttonStyle = {
     backgroundColor: props.color,
   };
-  return <button style={buttonStyle}>{props.text}</button>;
+  return <button style={{ buttonStyle }}>{props.text}</button>;
 }
