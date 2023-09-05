305cc36d291c77afc3a8007ebb465d3f4dc34cad
Hannah Hoang <hannahhoanggg@gmail.com>
2023-09-05 03:16:24 +0000

    Completed react-app-drawer-component exercise.

--- react-app-drawer-component/hannah-app/src/App.tsx
+++ react-app-drawer-component/hannah-app/src/App.tsx
@@ -2,19 +2,13 @@ import { useState } from 'react';
 import './App.css';
 import AppDrawer from './AppDrawer';
 
-const menuItems = [
-  'The Legend of Zelda',
-  'A Link to the Past',
-  'Ocarina of Time',
-  'The Wind Waker',
-  'Breath of the Wild',
-];
+const menuItems = ['About', 'Get Started', 'Sign In'];
 
 export default function App() {
   const [item, setItem] = useState('No menu selected');
   return (
     <div>
-      <AppDrawer header="Choose a Game" items={menuItems} onSelect={setItem} />
+      <AppDrawer header="Menu" items={menuItems} onSelect={setItem} />
       <h1>{item}</h1>
     </div>
   );
