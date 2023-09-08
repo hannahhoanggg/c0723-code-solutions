5a3607879e365e367015a61d3364299bb80c687f
Hannah Hoang <hannahhoanggg@gmail.com>
2023-09-08 21:23:16 +0000

    Edited some more code.

--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -3,10 +3,9 @@ import { writeFile } from 'node:fs/promises';
 async function writeRandomNumber() {
   try {
     const randomNumber = Math.random();
-    const randomString = randomNumber.toString();
-    await writeFile('random.txt', randomString + '\n');
+    await writeFile('random.txt', randomNumber + '\n');
   } catch (error) {
-    console.log('error: ', error.message);
+    console.log('error: ', error);
   }
 }
 writeRandomNumber();
