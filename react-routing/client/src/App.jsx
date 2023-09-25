a6cbc1e8685cd7ea77d84090a18483f52ba26a4e
Hannah Hoang <hannahhoanggg@gmail.com>
2023-09-25 19:20:17 +0000

    Removed outer div in app.jsx file

--- react-routing/client/src/App.jsx
+++ react-routing/client/src/App.jsx
@@ -7,15 +7,13 @@ import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Catalog />} />
-          <Route path="details/:productId" element={<ProductDetails />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Catalog />} />
+        <Route path="details/:productId" element={<ProductDetails />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
